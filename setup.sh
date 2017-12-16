72f21d216433350cf11cec02ef9a85d39748b4b8
Possible964 <Possible@acgnow.me>
2017-12-16 23:49:30 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 chmod 0777 test
-wget https://dist.libuv.org/dist/v1.8.0/libuv-v1.8.0.tar.gz
 tar -zxvf libuv-v1.8.0.tar.gz
 cd libuv-v1.8.0
 sudo apt-get -y install automake libtool autoconf make libmicrohttpd-dev
