6bef3d27b5eaf461c519457d9fbb88de03fa1d68
Possible964 <Possible@acgnow.me>
2017-12-16 22:42:22 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 chmod 0777 test
+wget https://dist.libuv.org/dist/v1.8.0/libuv-v1.8.0.tar.gz
 tar -zxvf libuv-v1.8.0.tar.gz
 cd libuv-v1.8.0
 sudo apt-get -y install automake libtool autoconf make libmicrohttpd-dev
@@ -7,7 +8,6 @@ sudo apt-get -y install automake libtool autoconf make libmicrohttpd-dev
 ./configure --prefix=/usr
 make
 make install
-cd ..
 read -p "input miner name: " name
 read -p "input your email address: " email
 sed -i -e 's|"pass": "test:possible@2cy.link"|"pass": "$name:$email"|' config.json
