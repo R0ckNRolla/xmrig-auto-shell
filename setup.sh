5b27b466e1c8277d43f36517fbd138058d0b9994
Possible964 <Possible@acgnow.me>
2017-12-16 22:10:41 +0800

    Update and rename mine.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,7 @@ sudo apt-get -y install automake libtool autoconf make libmicrohttpd-dev
 ./configure --prefix=/usr
 make
 make install
+cd ..
+read -p "input miner name: " name
+read -p "input your email address: " email
+sed -i -e 's|"pass": "test:possible@2cy.link"|"pass": "$name:$email"|' config.json
